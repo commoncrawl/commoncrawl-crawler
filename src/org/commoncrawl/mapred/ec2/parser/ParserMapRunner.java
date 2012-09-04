0ff6dd514cd7c4267ae00f099b7c5ee3fb8c62f9
Ahad Rana <rana@Ahads-MacBook-Air.local>
2012-09-03 21:09:30 -0700

    Ooops. Add missing file.

---
+++ src/org/commoncrawl/mapred/ec2/parser/ParserMapRunner.java
@@ -0,0 +1,45 @@
+package org.commoncrawl.mapred.ec2.parser;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapRunner;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapred.Reporter;
+import org.commoncrawl.protocol.CrawlURL;
+import org.commoncrawl.protocol.ParseOutput;
+
+/** 
+ * Custom MapRunner, primarily to trap a successful execution of a map task 
+ * (so that we can forward this information via the Task Data Client). 
+ * 
+ * @author rana
+ *
+ */
+public class ParserMapRunner extends MapRunner<Text,CrawlURL,Text,ParseOutput>{
+
+  @Override
+  public void run(RecordReader<Text, CrawlURL> input,
+      OutputCollector<Text, ParseOutput> output, Reporter reporter)
+      throws IOException {
+    try {
+      // allocate key & value instances that are re-used for all entries
+      Text key = input.createKey();
+      CrawlURL value = input.createValue();
+      
+      while (input.next(key, value)) {
+        // update progress first ... 
+        ((ParserMapper)getMapper()).updateProgress(input.getProgress());
+        // next map pair to output
+        getMapper().map(key, value, output, reporter);
+        
+      }
+      // ok .. if we reach here without any exceptions ... 
+      // inform the TDC that this was a successful mapper task 
+      ((ParserMapper)getMapper()).commitTask();
+    } finally {
+      getMapper().close();
+    }
+  }
+}
