0ccbe2f2bd3e99238203025bb7a6a21e9f7e0408
Ahad Rana <ahadrana@gmail.com>
2013-06-24 14:56:07 -0700

    Final Merge Support
    Add merge support and enhance command line parameters.

--- src/org/commoncrawl/mapred/ec2/postprocess/crawldb/CrawlDBIndexSearch.java
+++ src/org/commoncrawl/mapred/ec2/postprocess/crawldb/CrawlDBIndexSearch.java
@@ -1,14 +1,23 @@
 package org.commoncrawl.mapred.ec2.postprocess.crawldb;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nullable;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -19,25 +28,39 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DataInputBuffer;
 import org.apache.hadoop.io.DataOutputBuffer;
+import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.SequenceFile.CompressionType;
 import org.apache.hadoop.io.SequenceFile.ValueBytes;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.compress.SnappyCodec;
+import org.commoncrawl.hadoop.mergeutils.TextFileSpillWriter;
+import org.commoncrawl.mapred.ec2.postprocess.crawldb.CrawlDBMergeSortReducer.RawValueIterator;
 import org.commoncrawl.protocol.URLFPV2;
 import org.commoncrawl.util.CCStringUtils;
 import org.commoncrawl.util.KeyBasedSequenceFileIndex;
+import org.commoncrawl.util.MultiFileMergeUtils.MultiFileInputReader;
+import org.commoncrawl.util.MultiFileMergeUtils.MultiFileInputReader.KeyAndValueData;
+import org.commoncrawl.util.MultiFileMergeUtils.MultiFileInputReader.RawRecordValue;
 import org.commoncrawl.util.S3NFileSystem;
 import org.commoncrawl.util.TextBytes;
 import org.commoncrawl.util.Tuples.Pair;
 import org.commoncrawl.util.URLUtils;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -50,7 +73,8 @@ public class CrawlDBIndexSearch {
 
   static { 
     options.addOption(OptionBuilder.withArgName("indexpath").hasArg(true).isRequired().withDescription("Index Path").create("indexpath"));
-    options.addOption(OptionBuilder.withArgName("dbpath").hasArg(true).isRequired().withDescription("Database Path").create("dbpath"));
+    options.addOption(OptionBuilder.withArgName("dbpath").hasArg(true).withDescription("Database Path").create("dbpath"));
+    options.addOption(OptionBuilder.withArgName("dbpaths").hasArg(true).withDescription("Database Paths").create("dbpaths"));
     options.addOption(OptionBuilder.withArgName("domain").hasArg(true).isRequired().withDescription("Domain Name").create("domain"));
     options.addOption(OptionBuilder.withArgName("outputpath").hasArg(true).isRequired().withDescription("Output Path").create("outputpath"));
     options.addOption(OptionBuilder.withArgName("fileprefix").hasArg(true).withDescription("Index File Prefix").create("fileprefix"));
@@ -72,6 +96,9 @@ public class CrawlDBIndexSearch {
       
       throw e;
     }
+    finally {
+      System.exit(0);
+    }
   }
   
   private static FileSystem getFileSystemForPath(Path path,Configuration conf)throws IOException { 
@@ -86,6 +113,79 @@ public class CrawlDBIndexSearch {
     NUMBER_FORMAT.setGroupingUsed(false);
   }    
 
+  private static Function<TextBytes,String> createStandardTransformer() { 
+    final JsonParser parser = new JsonParser();
+    return new Function<TextBytes, String>() {
+
+      @Override
+      public String apply(final TextBytes arg0) {
+        JsonObject jsonObj = parser.parse(arg0.toString()).getAsJsonObject();
+        
+        ArrayList<String> fields = Lists.newArrayList();
+        
+        if (jsonObj != null) {
+          JsonObject blekkoStatus = jsonObj.getAsJsonObject(CrawlDBCommon.TOPLEVEL_BLEKKO_METADATA_PROPERTY);
+          if (blekkoStatus != null) {
+            // blekko url 
+            fields.add("1");
+            // blekko crawl status 
+            if (blekkoStatus.has(CrawlDBCommon.BLEKKO_METADATA_STATUS) 
+                && blekkoStatus.get(CrawlDBCommon.BLEKKO_METADATA_STATUS).getAsString().equalsIgnoreCase("crawled")) { 
+              fields.add("1");
+            }
+            else { 
+              fields.add("0");
+            }
+          }
+          else { 
+            // is blekko url 
+            fields.add("0");
+            // blekko crawled 
+            fields.add("0");
+          }
+          JsonObject crawlStatus = jsonObj.getAsJsonObject(CrawlDBCommon.TOPLEVEL_SUMMARYRECORD_PROPRETY);
+          JsonObject linkStatus = jsonObj.getAsJsonObject(CrawlDBCommon.TOPLEVEL_LINKSTATUS_PROPERTY);
+          if (crawlStatus != null || linkStatus != null) { 
+            // is cc url 
+            fields.add("1");
+            if (crawlStatus != null
+                && crawlStatus.get(CrawlDBCommon.SUMMARYRECORD_ATTEMPT_COUNT_PROPERTY).getAsInt() != 0) { 
+                // crawled ... 
+                fields.add("1");
+            }
+            else { 
+              // crawled ... 
+              fields.add("0");
+            }
+            if (linkStatus != null 
+                && linkStatus.has(CrawlDBCommon.LINKSTATUS_EXTRADOMAIN_SOURCES_COUNT_PROPERTY)
+                && linkStatus.get(CrawlDBCommon.LINKSTATUS_EXTRADOMAIN_SOURCES_COUNT_PROPERTY).getAsInt() != 0) { 
+              // ext hrefs
+              fields.add("1");
+            }
+            else { 
+              // ext hrefs 
+              fields.add("0");
+            }
+          }
+          else { 
+            // is cc url ... 
+            fields.add("0");
+            // crawled (not) 
+            fields.add("0");
+            // ext hrefs 
+            fields.add("0");
+          }
+        }
+        else { 
+          for (int i=0;i<5;++i)
+            fields.add("0");
+        }
+        return Joiner.on("\t").join(fields).toString();
+      }
+    };
+  }
+  
   private static void runQuery(CommandLine commandLine) throws IOException {
     
     final Configuration conf = new Configuration();
@@ -100,29 +200,62 @@ public class CrawlDBIndexSearch {
       = CrawlDBKey.generateMinMaxKeysForDomain(
           fp.getRootDomainHash(),
           (fp.getRootDomainHash() == fp.getDomainHash()) ? -1L:fp.getDomainHash());
-          
+    
+    Path dbPath = null;
     final Path indexPath = new Path(commandLine.getOptionValue("indexpath"));
-    final Path dbPath = new Path(commandLine.getOptionValue("dbpath"));
+
+    
+    Map<Integer,Pair<Path,Path>> shardToPathMap = Maps.newTreeMap();
+    
+    if (commandLine.hasOption("dbpath")) { 
+      dbPath = new Path(commandLine.getOptionValue("dbpath"));
+      FileSystem dbFS      = getFileSystemForPath(dbPath,conf);
+      FileStatus parts[] = dbFS.globStatus(new Path(indexPath,"part-*"));
+      for (FileStatus part : parts) { 
+        Path itemPath = part.getPath();
+        int shardIndex = Integer.parseInt(itemPath.getName().substring("part-".length()));
+        Pair<Path,Path> tuple = new Pair<Path,Path>(itemPath,new Path(indexPath,"index-" + NUMBER_FORMAT.format(shardIndex)));
+        shardToPathMap.put(shardIndex, tuple);
+      }
+    }
+    else if (commandLine.hasOption("dbpaths")) {
+      
+      Path partsFilePath = new Path(commandLine.getOptionValue("dbpaths"));
+      LOG.info("Parts File:" + partsFilePath);
+      
+      FSDataInputStream inputStream = FileSystem.get(partsFilePath.toUri(),conf).open(partsFilePath);
+      try { 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = null;
+        while ((line = reader.readLine()) != null) { 
+          Path itemPath = new Path(line);
+          int shardIndex = Integer.parseInt(itemPath.getName().substring("part-".length()));
+          Pair<Path,Path> tuple = new Pair<Path,Path>(itemPath,new Path(indexPath,"index-" + NUMBER_FORMAT.format(shardIndex)));
+          shardToPathMap.put(shardIndex, tuple);
+        }
+      }
+      finally { 
+        inputStream.close();
+      }
+    }
+     
+    if (shardToPathMap.size() == 0) { 
+      throw new IOException("No Valid Shards Specified!");
+    }
+    
     final Path outputPath = new Path(commandLine.getOptionValue("outputpath"));
-    final String indexFilePrefix = (commandLine.hasOption("fileprefix")?
-        commandLine.getOptionValue("fileprefix") : "index-");
-    final FileSystem indexFS   = getFileSystemForPath(indexPath,conf);
-    final FileSystem dbFS      = getFileSystemForPath(dbPath,conf);
     final FileSystem outputFS  = getFileSystemForPath(outputPath,conf);
 
-    // find out number for shard indexes... 
-    int partCount = indexFS.globStatus(new Path(indexPath,"part-*")).length;
-
-    if (partCount == 0) 
-      throw new IOException("Invalid Index Path:" + indexPath);
-    
     // for each part invoke a parallel query 
     List<Callable<String>> callables = Lists.newArrayList();
-    for (int i=0;i<partCount;++i) {
-      final int threadIndex = i;
-      
-      final Path indexFilePath = new Path(indexPath,indexFilePrefix + NUMBER_FORMAT.format(threadIndex));
-      final Path crawlDBFilePath = new Path(dbPath,"part-" + NUMBER_FORMAT.format(threadIndex));
+    for (Map.Entry<Integer,Pair<Path,Path>> entry : shardToPathMap.entrySet()) { 
+      final int threadIndex = entry.getKey();
+
+      final FileSystem indexFS   = getFileSystemForPath(entry.getValue().e1,conf);
+      final FileSystem dbFS      = getFileSystemForPath(entry.getValue().e0,conf);
+
+      final Path indexFilePath = entry.getValue().e1;
+      final Path crawlDBFilePath = entry.getValue().e0;
       final Path outputFilePath = new Path(outputPath,"part-" + NUMBER_FORMAT.format(threadIndex));
       final DataOutputBuffer minKeyOutputBuffer = new DataOutputBuffer();
       final DataOutputBuffer maxKeyOutputBuffer = new DataOutputBuffer();
@@ -147,7 +280,6 @@ public class CrawlDBIndexSearch {
   
             
             SequenceFile.Writer writer = SequenceFile.createWriter(outputFS, conf,outputFilePath, Text.class, Text.class,CompressionType.BLOCK,new SnappyCodec());
-            
             try { 
               // open a reader 
               LOG.info("Opening CrawlDB File at Path:" + crawlDBFilePath);
@@ -256,9 +388,13 @@ public class CrawlDBIndexSearch {
     // create the exector
     ExecutorService executor = Executors.newFixedThreadPool(500);
     LOG.info("Queueing " + callables.size() + " Work Items");
+    // for now, just standard transformer ... 
+    Function<TextBytes,String> valueTransformer = createStandardTransformer();
+    
     // execute queued items 
     try {
       List<Future<String>> futures = executor.invokeAll(callables);
+      mergeResults(conf,outputFS,outputPath,valueTransformer);
     } catch (InterruptedException e1) {
       // TODO Auto-generated catch block
       e1.printStackTrace();
@@ -272,6 +408,71 @@ public class CrawlDBIndexSearch {
       LOG.info("Execution Completed");
     } catch (InterruptedException e) {
       LOG.error("Execution Interrupted!");
+    }
+  }
+  
+  static void mergeResults(Configuration conf,FileSystem fs,Path outputPath,Function<TextBytes,String> valueTransformer)throws IOException { 
+    // collect parts 
+    FileStatus parts[] = fs.globStatus(new Path(outputPath,"part-*"));
+    List<Path> inputs = ImmutableList.copyOf(
+        Iterators.transform(Iterators.forArray(parts),new Function<FileStatus,Path>() {
+
+      @Override
+      @Nullable
+      public Path apply(@Nullable FileStatus arg0) {
+        return arg0.getPath();
+      } 
+      
+    }));
+    // feed to merger ... 
+    // set up merge attributes
+    Configuration localMergeConfig = new Configuration(conf);
+    // we don't want to use a grouping comparator because the we are using the reducer code from the intermediate 
+    // merge 
+    localMergeConfig.setClass(MultiFileInputReader.MULTIFILE_COMPARATOR_CLASS,TextBytes.Comparator.class, RawComparator.class);
+    localMergeConfig.setClass(MultiFileInputReader.MULTIFILE_KEY_CLASS,TextBytes.class, WritableComparable.class);
+    
+
+    Writer outputWriter = new OutputStreamWriter(fs.create(
+        new Path(outputPath,"results.txt"),
+        true,
+        1000000
+        ),Charset.forName("UTF-8"));
+
+    try { 
+      // initialize reader ... 
+      LOG.info("FileSystem is:" + fs.toString());
+      MultiFileInputReader<TextBytes> multiFileInputReader = new MultiFileInputReader<TextBytes>(fs, inputs, localMergeConfig);
+
+      try { 
+        RawValueIterator rawValueIterator = new RawValueIterator();
+        
+        Pair<KeyAndValueData<TextBytes>,Iterable<RawRecordValue>> nextItem = null;
+        
+        TextBytes valueText = new TextBytes();
+        DataInputBuffer inputBuffer = new DataInputBuffer();
+        
+        // walk tuples and feed them to the actual reducer ...  
+        while ((nextItem = multiFileInputReader.getNextItemIterator()) != null) {
+          for (RawRecordValue rawValue : nextItem.e1) { 
+            
+            inputBuffer.reset(rawValue.data.getData(), 0, rawValue.data.getLength());
+            valueText.setFromRawTextBytes(inputBuffer);
+            outputWriter.write(nextItem.e0._keyObject.toString()+"\t"+valueTransformer.apply(valueText)+"\n");
+          }
+        }
+      }
+      finally { 
+        multiFileInputReader.close();
+      }
+    }
+    finally {
+      try {
+        outputWriter.flush();
+      }
+      finally { 
+        IOUtils.closeStream(outputWriter);
+      }
     }
   }
   
