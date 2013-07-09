964aa994bbd5bbde9d72e08fadeabe73fddb24ba
Ahad Rana <ahadrana@gmail.com>
2013-07-08 19:22:00 -0700

    Use S3MultipartUploadStream in S3NFileSystem.
    Use new S3MultipartUploadStream as the underlying OutputStream for our
    version of the S3N FileSystem. S3MultipartUploadStream uses the Amazon
    multi-part protocol, and ,thus, does not need to pre-buffer the file to
    the local FS (like the original S3NFileSystem Impl) before initiating a
    transfer up to S3. Also, S3MultipartUploadStream is more resilient to
    failures (thanks to its use of the multi-part protocol).

--- src/org/commoncrawl/util/S3NFileSystem.java
+++ src/org/commoncrawl/util/S3NFileSystem.java
@@ -5,8 +5,11 @@ import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.fs.s3native.NativeS3FileSystem;
+import org.apache.hadoop.util.Progressable;
 
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -47,5 +50,36 @@ public class S3NFileSystem extends NativeS3FileSystem {
             f.toUri(), 
             getConf().get("fs.s3n.awsAccessKeyId"), 
             getConf().get("fs.s3n.awsSecretAccessKey"), bufferSize, 100));
+  }
+  
+  private Path makeAbsolute(Path path) {
+    if (path.isAbsolute()) {
+      return path;
+    }
+    return new Path(getWorkingDirectory(), path);
+  }
+
+  
+  @Override
+  public FSDataOutputStream create(Path f, FsPermission permission,
+      boolean overwrite, int bufferSize, short replication, long blockSize,
+      Progressable progress) throws IOException {
+
+    if (exists(f) && !overwrite) {
+      throw new IOException("File already exists:"+f);
+    }
+    Path absolutePath = makeAbsolute(f);
+    Path qualifiedPath = makeQualified(absolutePath);
+    
+    return new FSDataOutputStream(
+        new S3MultipartUploadStream(
+            qualifiedPath.toUri(), 
+            getConf().get("fs.s3n.awsAccessKeyId"),
+            getConf().get("fs.s3n.awsSecretAccessKey"),
+            overwrite, null, 
+            S3MultipartUploadStream.DEFAULT_UPLOADER_COUNT,
+            S3MultipartUploadStream.DEFAULT_PART_BUFFER_SIZE, 
+            Math.max(S3MultipartUploadStream.DEFAULT_MAX_QUEUED_COUNT, bufferSize / S3MultipartUploadStream.DEFAULT_PART_BUFFER_SIZE), 
+            S3MultipartUploadStream.DEFAULT_MAX_RETRIES_PER_PART), statistics);
   }
 }  
